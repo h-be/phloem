e75aecebf5fb2e8d137eab122fc53dbdf79d2406
Will Harris-Braun <willharrisbraun@gmail.com>
2019-06-04 12:58:53 -0400

    Change sample Widget ID

--- sample_config.rb
+++ sample_config.rb
@@ -3,6 +3,6 @@ CLIENT_SECRET = 'EDQnTsuCBivNEgrCOaDJ'                # Must be replaced from "m
 ACCESS_TOKEN = "723a9bc4-c729-620b-4f8d-d38fdc68a993" # From Miro
 BOARD_ID = "i9E_keXrQeL="                             # Found in board URL: https﻿://miro.com/app/board/[board id]/
 FRAMES = {                                            # Add entries for each Triage frame. Key: github repo name,
-  "translator"=>"3074457346676389664",                # value: id of frame you want to link repo to
+  "translator"=>"8937627738990371001",                # value: id of frame you want to link repo to
   "other-repo"=>"2474402776696584848"
 }
